3bacaa0210e90a2ced70a38651ece531a544cfa1
Estiuck Al Regun <earegun@users.noreply.github.com>
2018-04-08 14:46:29 +0600

    Create ad system.js

---
+++ ad system.js
@@ -0,0 +1,63 @@
+<center><script language="JavaScript1.1" type="text/javascript">
+<!-- 
+{
+gfx=new Array()
+wdh=new Array()
+hgt=new Array()
+lnk=new Array()
+
+gfx[0]="http://www.datashaping.com/RI_120x600_v4ani.gif"
+gfx[1]="http://www.datashaping.com/paw600.gif"
+gfx[2]="http://www.datashaping.com/paw600.gif" 
+gfx[3]="http://www.datashaping.com/rapidi_160x600.jpg"
+gfx[4]="http://datashaping.com/statsoftab.gif"
+gfx[5]="http://www.datashaping.com/rapidi_160x600.jpg" 
+gfx[6]="http://datashaping.com/statsoftab.gif"
+gfx[7]="http://www.datashaping.com/ericKing.jpg"
+gfx[8]="http://www.datashaping.com/ericKing.jpg"
+gfx[9]="http://www.datashaping.com/RI_120x600_v4ani.gif"
+
+
+lnk[0]="http://www.rapidinsightinc.com/"
+lnk[1]="http://www.predictiveanalyticsworld.com"
+lnk[2]="http://www.predictiveanalyticsworld.com" 
+lnk[3]="http://www.rapid-i.com?src=AB" 
+lnk[4]="http://www.youtube.com/user/StatSoft#g/c/B804A810436AFB03"
+lnk[5]="http://www.rapid-i.com?src=AB"  
+lnk[6]="http://www.youtube.com/user/StatSoft#g/c/B804A810436AFB03"
+lnk[7]="http://www.datashaping.com/redirectEricKing.html?src=AB"
+lnk[8]="http://www.datashaping.com/redirectEricKing.html?src=AB" 
+lnk[9]="http://www.rapidinsightinc.com/"
+
+
+wdh[0]="120"
+wdh[1]="160"
+wdh[2]="160"
+wdh[3]="120"
+wdh[4]="160"
+wdh[5]="120"
+wdh[6]="160"
+wdh[7]="120"
+wdh[8]="120"
+wdh[9]="120"
+
+
+hgt[0]="600"
+hgt[1]="600"
+hgt[2]="600"
+hgt[3]="600"
+hgt[4]="600"
+hgt[5]="600"
+hgt[6]="600"
+hgt[7]="600"
+hgt[8]="600"
+hgt[9]="600"
+
+
+rnd=Math.floor(Math.random()*10)
+
+document.writeln('<a href="'+lnk[rnd]+'"><IMG SRC="'+gfx[rnd]+'"  border=0 width='+wdh[rnd]+' height='+hgt[rnd]+'><\/a>')
+
+}
+//-->
+</script></center>
